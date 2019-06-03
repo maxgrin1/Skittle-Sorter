f2f274b0775c21a177bc8328e6e5c19cf28420d9
maxvangrinsven <42255180+maxgrin1@users.noreply.github.com>
2019-06-03 09:50:32 +0200

    Added comments + break when all skittles are sorted

--- SkittleSorter.py
+++ SkittleSorter.py
@@ -1,4 +1,6 @@
 #Sketch for determening the color of the Skittle and moving servo positions accordingly
+#Run calibrate() --> calDone = 0, first and put the gotten values in cal_values = [].
+#Sleep() statements should be changed to fit your Skittle Sorter.
 #By Luuk van Sundert and Max van Grinsven
 
 from picamera import PiCamera
@@ -17,12 +19,12 @@ bottomServo = GPIO.PWM(12, 50)
 topServo.start(8.5)
 bottomServo.start(7)
 camera = PiCamera()
-#Red, Orange, Purple, Yellow, Green
 calDone = 1
 #Fill in between the brackets the values gotten from calibrate()
 cal_values = [] 
 dominant_color = 0
-tree = spatial.KDTree(cal_values)
+if calDone:
+    tree = spatial.KDTree(cal_values)
 calString = "Insert "
 def getDominantColor():
     camera.capture('maxgrin1.jpg')
@@ -56,6 +58,9 @@ def getSkittle():
     elif b[1] == 4:
         print ("Green")
         bottomServo.ChangeDutyCycle(7)
+    elif b[1] == 5:
+        print ("No Skittles left)"
+        break
     sleep(0.3)
     topServo.ChangeDutyCycle(3.5)
     sleep(0.5)
@@ -64,7 +69,7 @@ def calibrate():
     average = []
     print ("Calibration: insert given color and press any button on keyboard to continue.")
     cv2.waitKey(0)
-    for i in range(5):
+    for i in range(6):
         if i == 0:
             print (calString + "Red")
             cv2.waitKey(0)
@@ -76,8 +81,10 @@ def calibrate():
             print (calString + "Yellow")
         elif i == 4:
             print (calString + "Green")
+        elif i == 5:
+            print (calString + "Empty")
         cv2.waitKey(0)
-        for k in range(10):
+        for k in range(10): #Range(K) K can be changed, the smaller the faster but less accurate.
             average.append(getDominantColor())
         averages = [sum(vals)/len(average) for vals in zip(*average)]
         averages = [round(x) for x in averages]
@@ -87,9 +94,10 @@ def calibrate():
     
 #The actual sketch itself
 if(calDone):
-    for a in range(10):
+    for a in range(10): #Range(K) K is the amount of Skittles to be sorted.
         getSkittle()
-
+else calibrate()
 cv2.destroyAllWindows()
 topServo.stop()
+bottomServo.stop()
 GPIO.cleanup()
